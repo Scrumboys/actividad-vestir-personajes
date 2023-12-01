d2143e70731dffbbe44f458e5515dfdc9d834cfb
Cebito03 <adrianloshjo@gmail.com>
2023-12-01 13:08:26 -0700

    Mezclar imagenes, resposive listos

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const nombres = ['Andres', 'Barbara', 'Nicolas', 'Victor', 'Luna', 'Mariana'];
 const iconos = Array.from(document.querySelectorAll('g.invisible'));
 const carouselButtons = document.querySelector('.carousel-buttons');
+const imgCabeza = ['cabezaAndres', 'cabezaBarbara','cabezaNicolas','cabezaLuna','cabezaVictor','cabezaMariana'];
+const imgTorzo = ['torzoAndres', 'torzoBarbara','torzoNicolas','torzoLuna', 'torzoVictor','torzoMariana'];
+const imgPiernas =['piernasAndres','piernasBarbara','piernasNicolas','piernasLuna','piernasVictor','piernasMariana'];
+
 
 carouselButtons.addEventListener('click', () => {
     setTimeout(() => {
@@ -22,7 +26,30 @@ function validarImagenes([pierna, torzo, cabeza]) {
         }
     });
 }
+function mezclarImagenes() {
+    // Obtén una imagen aleatoria de cada arreglo
+    const cabezaAleatoria = imgCabeza[Math.floor(Math.random() * imgCabeza.length)];
+    const torzoAleatorio = imgTorzo[Math.floor(Math.random() * imgTorzo.length)];
+    const piernasAleatorias = imgPiernas[Math.floor(Math.random() * imgPiernas.length)];
+
+    const cabezaActiva = document.getElementById(cabezaAleatoria);
+    document.getElementById("cabezaAndres").classList.remove("active");
+    cabezaActiva.classList.add("active");
+
+    const torzoActivo = document.getElementById(torzoAleatorio);
+    document.getElementById("torzoAndres").classList.remove("active");
+    torzoActivo.classList.add("active");
+
+    const piernasActiva = document.getElementById(piernasAleatorias);
+    document.getElementById("piernasAndres").classList.remove("active");
+    piernasActiva.classList.add("active");
+
+    // Combina las tres imágenes
+    const imagenFinal = cabezaAleatoria + ' ' + torzoAleatorio + ' ' + piernasAleatorias;
+    // Muestra la imagen final en la consola (puedes ajustar esto según tus necesidades)
+    console.log(imagenFinal);
+    }
 
 iconos.forEach(icono => icono.classList.add('transition'));
-
+mezclarImagenes();
 
